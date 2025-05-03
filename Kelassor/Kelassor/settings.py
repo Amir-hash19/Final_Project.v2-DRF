06af9854223b98eafffefd4f121dd0e6e364d52d
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-05-03 15:38:29 +0330

    remove secret data

--- Kelassor/Kelassor/settings.py
+++ Kelassor/Kelassor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_$5f!n!r&t*66c!psho%61tq$mn6!bx4*tp9s%fm5c3bu(tnte'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,9 +85,9 @@ WSGI_APPLICATION = 'Kelassor.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "kelassor",
-        "USER": "admin",
-        "PASSWORD": "amir112233",
+        "NAME": config('DB_NAME'),
+        "USER": config('DB_USER'),
+        "PASSWORD": config('DB_PASSWORD'),
         "HOST": "localhost",
         "PORT": "5432",
     }
