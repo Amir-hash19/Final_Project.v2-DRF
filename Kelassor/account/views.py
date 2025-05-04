1596a6d48f94e566c075158d44f90d30aec78ce7
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-05-04 11:09:13 +0330

    add Permissions

--- Kelassor/account/views.py
+++ Kelassor/account/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import CustomUser
 
-# Create your views here.
+
+
+
+
