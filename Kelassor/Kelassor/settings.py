452ad85d25de68ddddafd52aea90b224c3d476ee
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2025-05-04 12:01:24 +0330

    config celery

--- Kelassor/Kelassor/settings.py
+++ Kelassor/Kelassor/settings.py
@@ -140,7 +140,16 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',  # استفاده از JWT
     ],
-}    
+    "DEFAULT_THROTTLE_RATES": {
+        "otp": "5/hour",
+        },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+
+}   
+ 
 
 
 
@@ -152,3 +161,17 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=4), 
 }
+
+
+
+
+
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+
