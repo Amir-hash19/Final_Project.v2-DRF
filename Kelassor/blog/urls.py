13a52d5c7145e06a013ec4ce679160587b993769
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-05-04 23:18:20 +0330

    create delete category blog added

--- Kelassor/blog/urls.py
+++ Kelassor/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AddCategoryBlogView, UploadBlogView
+from .views import AddCategoryBlogView, UploadBlogView, DeleteCategoryBlogView
 
 
 
 urlpatterns = [
     path("add-category/", AddCategoryBlogView.as_view(), name="create-category-blog"),
     path("upload-blog/", UploadBlogView.as_view(), name="create-blog"),
+    path("delete-category/<int:pk>", DeleteCategoryBlogView.as_view(), name="delete-category-blog")
 ]
