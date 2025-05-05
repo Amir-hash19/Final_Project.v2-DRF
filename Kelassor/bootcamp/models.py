25c61c032a31cec92ef35bf84b91fa957d13e596
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-05-05 12:24:37 +0330

    create bootcamp added

--- Kelassor/bootcamp/models.py
+++ Kelassor/bootcamp/models.py
@@ -2,6 +2,7 @@ from account.models import CustomUser
 from django.db import models
 from django.core.exceptions import ValidationError
 from phonenumber_field.modelfields import PhoneNumberField
+from django.utils.text import slugify
 
 
 
@@ -32,7 +33,7 @@ class Bootcamp(models.Model):
     instructor = models.ManyToManyField(to=CustomUser, related_name="teachers")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_online = models.BooleanField(default=True)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     category = models.ForeignKey(to=BootcampCategory, on_delete=models.CASCADE, null=True ,related_name="bootcamp_list")
     capacity = models.PositiveIntegerField()
     description = models.TextField()
@@ -42,6 +43,18 @@ class Bootcamp(models.Model):
     hours = models.CharField(max_length=50)
     days = models.CharField(max_length=50)
     slug = models.SlugField(unique=True)
+
+    def post(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            unique_slug = base_slug
+            counter = 1
+            while Bootcamp.objects.filter(slug=unique_slug).exists():
+                unique_slug = f"{base_slug}-{counter}"
+                counter+=1
+            self.slug = unique_slug
+            super().save(*args, **kwargs)
+
 
     def __str__(self):
         return self.title
