a13a814b9380526de804850363805cdfb0175801
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-05-12 15:17:56 +0330

    add task for call user for deadline

--- Kelassor/finance/tasks.py
+++ Kelassor/finance/tasks.py
@@ -2,8 +2,8 @@ from celery import shared_task
 from django.conf import settings
 from django.core.cache import cache
 from kavenegar import KavenegarAPI
+from .models import Invoice
 import os
-
 
 api = KavenegarAPI(settings.KAVENEGAR_API_KEY)
 
@@ -32,3 +32,30 @@ def send_sms_for_invoice_task(self, phone_client, last_name):
     
 
 
+
+@shared_task(bind=True, max_retries=3, default_retry_delay=5, ignore_result=True)
+def notify_when_deadline(self, phone, name):
+    from django.utils.timezone import localdate
+    today = localdate()
+    
+    invoices = Invoice.objects.filter(is_paid=False, deadline__lt=today)
+    
+    for invoice in invoices:
+        user = invoice.client
+        phone = str(user.phone) 
+        name = invoice.client.get_full_name()
+        
+        message = f"Dear {name}, Amount: {invoice.amount} please check your panel"    
+        
+        params = {
+            "receptor":phone,
+            "message":message,
+            "sender":"2000660110"
+        }
+        try:
+            response = api.sms_send(params)
+            if response.get('status') != 200:
+                raise Exception("SMS sending failed.")
+        except Exception as e:
+            raise self.retry(exc=e)    
+    return f"SMS notifications sent successfully"
