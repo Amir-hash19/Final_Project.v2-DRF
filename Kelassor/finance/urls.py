0cfe0be6dd6275590e86bd031c6c25dae2801425
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-05-12 14:59:35 +0330

    create payment added

--- Kelassor/finance/urls.py
+++ Kelassor/finance/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateInvoiceView, ListUserWithInvoiceView, DeleteInvoiceView
+from .views import CreateInvoiceView, ListUserWithInvoiceView, DeleteInvoiceView, CreatePaymentView
 
 
 urlpatterns = [
     path("create-invoice/", CreateInvoiceView.as_view(), name="create-invoice-for-user-by-admin"),
     path("list-user-invoices/", ListUserWithInvoiceView.as_view(), name="list-users-with-invoices"),
     path("delete-invoice/<slug:slug>/", DeleteInvoiceView.as_view(), name="delete-invoice-by-admin"),
+    path("create/<slug:slug>/payment/", CreatePaymentView.as_view(), name="create-payment"),
 ]
