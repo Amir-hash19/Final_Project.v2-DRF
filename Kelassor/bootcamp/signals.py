741cfb331de4d6f5888890cd0fb61ab8113e70b6
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-05-13 15:35:49 +0330

    add auto creator slug added to all signals file

--- Kelassor/bootcamp/signals.py
+++ Kelassor/bootcamp/signals.py
@@ -1,8 +1,42 @@
 from django.db.models.signals import post_save, pre_save
-from .models import BootcampRegistration
+from .models import BootcampRegistration, Bootcamp, BootcampCategory
 from django.dispatch import receiver
+from django.utils.text import slugify
 from .tasks import send_sms_to_user
 from django.db.models import F
+from datetime import datetime
+import uuid
+
+
+
+
+@receiver(post_save, sender=Bootcamp)
+def generate_slug_bootcamp(sender, instance, **kwargs):
+    if not instance.slug:
+        base_slug = slugify(instance.title)
+        unique_suffix = str(uuid.uuid4())[:8]
+        instance.slug = slugify(f"{base_slug}-{unique_suffix}")
+
+
+
+
+@receiver(post_save, sender=BootcampRegistration)
+def generate_slug_registration(sender, instance, **kwargs):
+    if not instance.slug:
+        date_part = datetime.now().strftime("%Y%m%d")
+        uuid_part = str(uuid.uuid4())[:8]
+        instance.slug = slugify(f"b-registration-{date_part}-{uuid_part}")
+
+
+
+
+@receiver(post_save, sender=BootcampCategory)
+def generate_slug_category(sender, instance, **kwargs):
+    if not instance.slug:
+        base_slug = slugify(instance.name)
+        unique_suffix = str(uuid.uuid4())[:8]
+        instance.slug = slugify(f"{base_slug}-{unique_suffix}")
+
 
 
 
@@ -13,9 +47,6 @@ def check_capacity_bootcamp(sender, instance, created, **kwargs):
         if bootcamp.capacity > 0:
             bootcamp.capacity -= 1
             bootcamp.save()
-
-
-
 
 
 
