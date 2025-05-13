ce1337c0d731bc40df79caea9e127f8a1fe15e56
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2025-05-13 15:35:49 +0330

    add auto creator slug added to all signals file

--- Kelassor/account/signals.py
+++ Kelassor/account/signals.py
@@ -2,7 +2,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import CustomUser
 from .tasks import send_welcome_sms_task
-
+from django.utils.text import slugify
+from datetime import datetime
+import uuid
 
 
 
@@ -16,3 +18,11 @@ def send_welcome_message(sender, created, instance, **kwargs):
         send_welcome_sms_task.delay(last_name, user_phone_number)
         
 
+
+
+@receiver(post_save, sender=CustomUser)
+def generate_slug_customeuser(sender, instance, **kwargs):
+    if not instance.slug:
+        base_slug = slugify(instance.username)
+        unique_suffix = str(uuid.uuid4())[:8]
+        instance.slug = slugify(f"{base_slug}-{unique_suffix}")
